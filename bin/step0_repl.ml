8c697e2bd151848f1e6daef581d3d0cdb7ff475b
Ratakor <ratakor@disroot.org>
2025-12-14 16:39:35 +0100

    step0_repl: follow pseudocode

--- bin/step0_repl.ml
+++ bin/step0_repl.ml
@@ -1,15 +1,14 @@
 open Printf
 
-let read s = s
-let eval s = s
-let print s = s
-let rep s = s |> read |> eval |> print
+let read str = str
+let eval _env ast = ast
+let print exp = exp
+let rep str = str |> read |> eval "" |> print
 
 let () =
   try
     while true do
       printf "user> %!";
-      let line = read_line () in
-      rep line |> printf "%s\n"
+      read_line () |> rep |> printf "%s\n"
     done
   with End_of_file -> printf "\n"
